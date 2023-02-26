fbb6a03bb627b4fa526954f0b6f4933034bac84b
MikhailoA <mihasdriller1987@gmail.com>
2023-02-26 23:49:36 +0100

    Градуси в радіани Радіани в градуси

--- Lesson 2/Home work 24.02.23.py
+++ Lesson 2/Home work 24.02.23.py
@@ -1,12 +1,12 @@
 # convertation radians to degrees
 
 pi = 3.14159265359
-radian = float(input("Input radians: "))
+radian = float(input("Введіть радіани: "))
 degree = radian * (180 / pi)
 print(round(degree, 5))
 
 # convertation degrees to radians
 
-degree = float(input("Input degree: "))
+degree = float(input("Введіть градуси: "))
 radian = degree * (pi / 180)
 print(round(radian, 5))
