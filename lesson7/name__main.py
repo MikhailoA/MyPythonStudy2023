25052fcbeaf624627b61672ee957a84756931ba0
MikhailoA <mihasdriller1987@gmail.com>
2023-03-20 18:07:42 +0100

    фінальна версія

--- lesson7/name__main.py
+++ lesson7/name__main.py
@@ -1,12 +1,32 @@
 # Програма повторює будь яку вашу вправу з HW 1. Вправи за типами даних int та float або HW 2.
-# convertation radians to degrees
-def convert_radian_to_degree(radian):
+# конвертації радіан в градуси та градусів в радіани.
+
+def convert_radians_to_degrees(radians):  # функція конвертації радіан в градуси.
     pi = 3.14159265359
-    degree = radian * (180 / pi)
-    return round(degree, 5)
+    degrees = radians * (180 / pi)
+    return round(degrees, 5)
+
+
+def convert_degrees_to_radians(degrees):  # функція конвертації градусів в радіани.
+    pi = 3.14159265359
+    radians = degrees * (pi / 180)
+    return round(radians, 5)
 
 
 if __name__ == '__main__':
-    radian = float(input("Введіть радіани: "))
-    degree = convert_radian_to_degree(radian)
-    print(f'Градуси: {degree}')
+    print("Виберіть конвертацію:")  # діалог з користувачем.
+    print("1. Радіани у градуси")
+    print("2. Градуси у радіани")
+
+    choice = int(input("Зробіть вибір (1 або 2): "))
+
+    if choice == 1:
+        radians = float(input("Введіть радіани: "))
+        degrees = convert_radians_to_degrees(radians)
+        print(f"{radians} радіан = {degrees} градусів")
+    elif choice == 2:
+        degrees = float(input("Введіть градуси: "))
+        radians = convert_degrees_to_radians(degrees)
+        print(f"{degrees} градусів = {radians} радіан")
+    else:
+        print("Не вірний вибір")
