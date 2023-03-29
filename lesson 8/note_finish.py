abd19f5568043eccd9ffd87d31e19fa862e650dd
MikhailoA <mihasdriller1987@gmail.com>
2023-03-29 18:36:21 +0200

    фінальна версія

---
+++ lesson 8/note_finish.py
@@ -0,0 +1,60 @@
+# Функція, що додає нотатки
+def add_note():
+    note = input('Введіть текст нотатки: ')
+    notes.append(note)
+
+
+# Функція, що виводить найраніші нотатки
+def earliest_notes():
+    n = input('Скільки нотаток ви бажаєте побачити? ')
+    n = int(n)
+    for note in notes[:n]:
+        print(note)
+
+
+# Функція, що виводить найпізніші нотатки
+def latest_notes():
+    n = input('Скільки нотаток ви бажаєте побачити? ')
+    n = int(n)
+    for note in notes[-n:]:
+        print(note)
+
+
+# Функція, що виводить нотатки за зменшенням довжини
+def longest_notes():
+    n = input('Скільки нотаток ви бажаєте побачити? ')
+    n = int(n)
+    sorted_notes = sorted(notes, key=len, reverse=True)
+    for note in sorted_notes[:n]:
+        print(note)
+
+
+# Функція, що виводить нотатки по зростанню довжини
+def shortest_notes():
+    n = input('Скільки нотаток ви бажаєте побачити? ')
+    n = int(n)
+    sorted_notes = sorted(notes, key=len)
+    for note in sorted_notes[:n]:
+        print(note)
+
+
+# Функція, яка пропонує користувачу вводити команди та опрацьовує їх
+if __name__ == '__main__':
+    notes = []  # Список для зберігання нотаток
+    while True:
+        command = input('Введіть команду (add, earliest, latest, longest, shortest, exit): ')
+        if command == 'add':
+            add_note()
+        elif command == 'earliest':
+            earliest_notes()
+        elif command == 'latest':
+            latest_notes()
+        elif command == 'longest':
+            longest_notes()
+        elif command == 'shortest':
+            shortest_notes()
+        elif command == 'exit':
+            print('До зустрічі!!!')
+            break
+        else:
+            print('Некоректна команда. Спробуй ще раз.')
